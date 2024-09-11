fa63e3e5d3384410cc169c33501670e62607720a
kgary432 <kgary7373@gmail.com>
2024-09-10 23:18:12 -0400

    optimized find duplicates to no longer use nested loops

--- java/FindDuplicates.java
+++ java/FindDuplicates.java
@@ -1,22 +1,23 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class FindDuplicates {
 
-    public static List<Integer> findModeNestedLoops(List<Integer> l) {
-        List<Integer> duplicates = new ArrayList<>();    
-        
-        for (int i=0; i<l.size(); i++) {
-            for (int j=1; j<l.size()+1; j++) {
-                if (l.get(i).equals(l.get(j))) {
-                    duplicates.add(l.get(i));
-                    break;
-                }
+    public static List<Integer> findMode(List<Integer> l) {
+        HashSet<Integer> seen = new HashSet<>();
+        List<Integer> duplicates = new ArrayList<>();
+    
+        for (int i = 0; i < l.size(); i++) {
+            if (seen.contains(l.get(i))) {
+                duplicates.add(l.get(i));
+            } else {
+                seen.add(l.get(i));
             }
         }
         return duplicates;
-    }
+}
 
     public static void main(String[] args) {
         // some test strings:
@@ -24,10 +25,10 @@ public class FindDuplicates {
         List<Integer> sample2 = new ArrayList<Integer>(Arrays.asList(3, 5, 6, 4, 4, 5, 66, 6, 7, 6));
         List<Integer> sample3 = new ArrayList<Integer>(Arrays.asList(3, 0, 5, 1, 0));
         List<Integer> sample4 = new ArrayList<Integer>(Arrays.asList(3));
-        System.out.println("Sample 1: " + findModeNestedLoops(sample1));
-        System.out.println("Sample 2: " + findModeNestedLoops(sample2));
-        System.out.println("Sample 3: " + findModeNestedLoops(sample3));
-        System.out.println("Sample 4: " + findModeNestedLoops(sample4));
+        System.out.println("Sample 1: " + findMode(sample1));
+        System.out.println("Sample 2: " + findMode(sample2));
+        System.out.println("Sample 3: " + findMode(sample3));
+        System.out.println("Sample 4: " + findMode(sample4));
     }
 
 }
--- python/find_duplicates.py
+++ python/find_duplicates.py
@@ -4,12 +4,16 @@ def find_duplicates_nested_loop(l: list) -> list:
     # make new list to append into
     duplicates = []
     # nested loop 1
-    for i in range(len(l)):
-        for j in range(i+1, len(l)):
-            if l[i] == l[j]:
+    def findMode(l):
+        seen = set()
+        duplicates = []
+        for i in range(len(l)):
+            if l[i] in seen:
                 duplicates.append(l[i])
-                break
-    return duplicates
+            else:
+                seen.add(l[i])
+        return duplicates
+
 
 if __name__ == "__main__":
     sample1 = [3, 7, 5, 6, 7, 4, 8, 5, 7, 66]
